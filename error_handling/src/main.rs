7cdffc4f706c1b301c24d39c35bda293d9664e9f
ITHwang <intaek428@gmail.com>
2023-12-11 12:14:14 +0000

    finish ch9

--- error_handling/src/main.rs
+++ error_handling/src/main.rs
@@ -1,3 +1,13 @@
+use std::error::Error;
+
+// Box<dyn Error>: trait object which means "any kind of error"(Chap 17)
+/*
+When a main function returns `Result<(), E>`, 
+exit with a value of 0 if main returns Ok.
+exit with a nonzero value if main returns Err.
+*/
+
+// fn main() -> Result<(), Box<dyn Error>> {
 fn main() {
     /*
     Rust groups errors into two categories:
@@ -57,10 +67,134 @@ fn main() {
     // expect
     // Unlike `unwrap`, `expect` can provide a custom error message.
     // `expect` can give more context than `unwrap`.
-    let greeting_file = File::open("hello.txt").expect("Problem opening the file");
+    // let greeting_file = File::open("hello.txt").expect("Problem opening the file");
 
     /*
-    3. Propagating errors
+    3. Propagating errors(returning an error to the calling code)
     */
+    use std::io::{self, Read};
+    
+    fn read_username_from_file() -> Result<String, io::Error> {
+        let username_file_result = File::open("hello.txt");
+
+        let mut username_file = match username_file_result {
+            Ok(file) => file,
+            Err(error) => return Err(error), // return the error to the calling code
+        };
+
+        let mut username = String::new();
+
+        // finally return Ok or Err variants.
+        match username_file.read_to_string(&mut username) {
+            Ok(_) => Ok(username),
+            Err(error) => Err(error),
+        }
+    }
+    // When the function provides return values,
+    // it's up to the calling code to decide what to do with those values.
+    // So we propagate all the success or error information upward for it to handle appropriatly.
+
+    // the `?` operator
+    // if the function returns Ok, it will return the value inside the Ok.
+    // if the function returns Err, it will return the error inside the Err.
+    fn read_username_from_file2() -> Result<String, io::Error> {
+        let mut username_file = File::open("hello.txt")?;
+        let mut username = String::new();
+        username_file.read_to_string(&mut username)?;
+        Ok(username)
+    }
+    // Error values that have the `?` operator go through the `from` function, defined in the `From` trait.
+    // When the `?` calls the `from` function, the error value is converted into the error type defined in the return type.
+    // This is useful when a function returns one error type.
+
+    // shorten by chaining method calls
+    fn read_username_from_file3() -> Result<String, io::Error> {
+        let mut username = String::new();
+        File::open("hello.txt")?.read_to_string(&mut username)?;
+        Ok(username)
+    }
+    // Already implemented
+    use std::fs;
+        
+    fn read_username_from_file4() -> Result<String, io::Error> {
+        fs::read_to_string("hello.txt")
+    }
+
+    // To use `?` in a function, the function must return a `Result` or  a `Option`.
+    // And you can't mix and match `Result` and `Option`.
+    fn last_char_of_first_line(text: &str) -> Option<char> {
+        text.lines().next()?.chars().last()
+    }
+
+    // causes the error
+    // let greeting_file = File::open("hello.txt")?;
+    // Ok(())
+
+    /*
+    4. To panic! or Not to panic!
+    */
+
+    /*
+    When to panic!
+    - There's no way to recover.
+    - A bad state(some assumption, guarantee, contract, or invariant has been broken)
+    - A failure is unexpected.
+    - An input of a function is invalid.
+    */
+
+    /*
+    When not to panic! -> use `Result`
+    - a good default choice when defining a function.
+    - Let the calling code decide whether to panic.
+    - A failure is expected.
+    */
+
+    /*
+    When to use `unwrap` or `expect`
+    - very handy when prototyping, before you're ready to decide how to handle errors.
+    - when we ensure the `Result` will have an Ok value but the compiler can't understand why(must document the reason)
+    */
+    use std::net::IpAddr;
+    let home: IpAddr = "127.0.0.1" // the compiler isn't smart enough to know that this is valid.
+        .parse()
+        .expect("Invalid IP address");
+
+    // 4. Custom types for validation
+    // Having lots of error check is verbose and annoying.
+    // use Rust's type system to simplify the error check code.
+
+    // loop {
+    //     // --snip--
+    //     let guess: i32 = match guess.trim().parse() {
+    //         Ok(num) => num,
+    //         Err(_) => continue,
+    //     };
+    //     if guess < 1 || guess > 100 {
+    //         println!("Out of range!");
+    //         continue;
+    //     }
+
+    //     match guess.cmp(&secret_number) {
+    //         // --snip--
+    //     }
+    // }
+
+    pub struct Guess {
+        value: i32,
+    }
+
+    impl Guess {
+        pub fn new(value: i32) -> Guess {
+            if value < 1 || value > 100 {  // check whether to violate the contract
+                panic!("Out of range: {}.", value);
+            }
+
+            Guess{ value }
+        }
+
+        pub fn value(&self) -> i32 { // self.value is private.
+            self.value
+        }
+    }
     
 }
