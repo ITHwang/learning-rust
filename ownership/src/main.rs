7cc7e8afd4fa2b76e6661ed212192c3f2eb2433a
ITHwang <intaek428@gmail.com>
2023-12-14 14:33:41 +0000

    update ch4_ownership

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,3 +1,30 @@
 fn main() {
-    println!("Hello, world!");
+    /*
+    'Ownership' is a set of rules that govern how data is managed in Rust.
+    */
+
+    /*
+    1. Stack and Heap
+    - All data stored on the stack must have a known, fixed size.
+    - Data with an unknown size at compile time or a size that might change must be stored on the heap.
+    - Main purpose of ownership is to manage heap data as well as stack data.
+    */
+
+    /*
+    2. Ownership Rules
+    1) Each value in Rust has an owner.
+    2) There can only be one owner as a time.
+    3) When the owner goes out of scope, the value will be dropped.
+    */
+
+    /*
+    3. Memory & Allocation
+    */
+
+    /*
+    4. Ownership & Functions
+    ...
+    */
+    
+    
 }
