97057c5e91c7353d6c8affb7798dba6cf746a178
ITHwang <intaek428@gmail.com>
2023-12-15 14:10:42 +0000

    todo: dangling references

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -19,12 +19,46 @@ fn main() {
 
     /*
     3. Memory & Allocation
+    - When a variable goes out of scope, Rust calls the `drop` function,
+    - but in the case when variables of the primitive types go out of the scope, Rust drop the values without calling the `drop`.
+    - The `drop` func is familar to 'Resource Acquisition Is Initialization'(RAII) in C++.
+    - The values saved on the heap memory like String, is made up of three parts:
+        1) A pointer to the memory that holds the contents
+        2) A length
+        3) A capacity
+    */
+
+    let s1 = String::from("hello");
+    let s2 = s1; // moved to s2.(s1 is invalid.)
+
+    let s1 = String::from("hello");
+    let s2 = s1.clone(); // deepcopy
+
+    // If a type implements the `Copy` trait, variables are not moved, but deep-copied.
+    // The primitive types basically implement the `Copy` trait.
+
+    /*
+    4. References & Borrowing
+    - Rust has a feature for using a value without transferring ownership. -> 'References'
+    - A reference is like a pointer in that it's an address we can follow to access the data stored at that address.
+    - Unlike a pointer, a reference is guaranteed to point to a valid value of a particular type.
+    - A mutable reference allows us to modify a borrowed value.
     */
 
     /*
-    4. Ownership & Functions
-    ...
+    Mutable References
+
+    - If you have a mutable reference to a value, you can have no other references to that value.
+    - This is for preventing data races at compile time.
+    - Note: a reference's scope starts from where it is introduced
+            and continues through the last time that reference is used.
     */
-    
-    
+
+    /*
+    Dangling References
+    */
+
+    /*
+    5. The Slice Type
+    */
 }
