4f977e9c548fac626f409def455463f9d9886d3b
ITHwang <intaek428@gmail.com>
2023-12-29 04:04:30 +0000

    packages&crates

--- ch7_module_system/src/main.rs
+++ ch7_module_system/src/main.rs
@@ -1,3 +1,20 @@
 fn main() {
-    println!("Hello, world!");
+    /*
+    1) The module system: 
+        packages(a cargo feature), 
+        crates(a tree of modules), 
+        modules
+        paths
+
+    2) Concepts
+    - Crate: the smallest amount of code that the Rust compiler considers at a time.
+        - Binary crates: programs you can compile to an executable.
+        - Library crates: functionality intended to be shared with multiple projects.
+    - Crate root: a source file that the Rust compiler starts from.
+    - Package: a bundle of one or more crates that provides a set of functionality.
+        - can contain multiple binary crates but only one library crate.
+        - must contain at least one crate.
+    */
+
+    // todo: Defining Modules
 }
