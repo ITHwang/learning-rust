b18694d8868e542704ea214feae9f3b439acdd6b
ITHwang <intaek428@gmail.com>
2023-12-29 07:53:49 +0000

    finished ch7

--- ch7_module_system/src/main.rs
+++ ch7_module_system/src/main.rs
@@ -1,9 +1,11 @@
 fn main() {
     /*
+    1. Modules
+    
     1) The module system: 
         packages(a cargo feature), 
         crates(a tree of modules), 
-        modules
+        modules(readability, easy-reuse, control the privacy)
         paths
 
     2) Concepts
@@ -15,6 +17,28 @@ fn main() {
         - can contain multiple binary crates but only one library crate.
         - must contain at least one crate.
     */
+    
+    /*
+    2. Defining modules
 
-    // todo: Defining Modules
+    - Start from the crate root: `src/main.rs` or `src/lib.rs`
+    - Declaring modules: src/{module_name}.rs
+    - Declaring submodules: src/{module_name}/{submodule_name}.rs
+    - the `use` keyword
+        - shortcut(a symbolic link in the filesystem)
+        - Specifying the parent module when calling the function makes it clear that the function isn't locally defined.
+    - An absolute path is better, because we tend to move code independently.
+    - The `super` keyword let us have fewer places to update code in the future.
+    - The `as` keyword: We can specify `as` and a new local name(alias).
+    - The `pub` keyword
+        - In Rust, items in a parent module can't use the private items inside child modules.
+        - But items in child modules can use the items in their ancestor modules.
+        - The `pub` keyword exposes inner parts of child modules' code.
+    - Public Enums: Unlike a struct, if we make an enum public, all of its variants are public.
+    - The `pub use` keywords: We're bringing an item into scope, 
+        but also making that item available for others to bring into their scope.("Re-exporting")
+    - Packages with a Binary and a Library
+        - The module tree should be defined in `src/lib.rs`.
+        - The binary crate becomes a user who uses any public items in the library crate just like the public API.
+    */
 }
