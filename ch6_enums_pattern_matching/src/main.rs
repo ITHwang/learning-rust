c58ad92765307eb7b49abe974cd3f3dd65c61d88
ITHwang <hwangintech@gmail.com>
2024-08-29 03:11:01 +0900

    upto ch6

--- ch6_enums_pattern_matching/src/main.rs
+++ ch6_enums_pattern_matching/src/main.rs
@@ -1,57 +1,61 @@
 fn main() {
     // 1. Enumerations(enums)
-    
+
     /*
-    - Enums allows you to define a type by enumerating its possible "variants". 
+    - Enums allows you to define a type by enumerating its possible "variants".
     - An enum value can only be one of its variants.
     */
     enum IpAddrKind {
         V4,
         V6,
     }
-    let four = IpAddrKind::V4;
-    let six = IpAddrKind::V6;
+    let _four = IpAddrKind::V4;
+    let _six = IpAddrKind::V6;
 
     /*
     - We can put data directly into each enum variant.
     - The name of each enum variant also becomes a constructor.
     - Each variant can have different types and amounts of associated data.
     */
-    enum IpAddr {
-        V4(u8, u8, u8, u8),
-        V6(String),
-    }
-    let home = IpAddr::V4(127, 0, 0, 1);
-    let loopback = IpAddr::V6(String::from("::1"));
+    // enum IpAddr {
+    //     V4(u8, u8, u8, u8),
+    //     V6(String),
+    // }
+    // let _home = IpAddr::V4(127, 0, 0, 1);
+    // let _loopback = IpAddr::V6(String::from("::1"));
 
     // Another example
-    struct QuitMessage;                        // unit struct 
-    struct MoveMessage {                       // normal struct
+    struct _QuitMessage; // unit struct
+    struct _MoveMessage {
+        // normal struct
         x: i32,
         y: i32,
     }
-    struct WriteMessage(String);               // tuple struct
-    struct ChangeColorMessage(i32, i32, i32);  // tuple struct
+    struct _WriteMessage(String); // tuple struct
+    struct _ChangeColorMessage(i32, i32, i32); // tuple struct
 
     // If we used the different structs,
     // we couldn't easily define a function to take these massages.
     // Instead,
     #[derive(Debug)]
     enum Message {
-        Quit,
-        Move { x: i32, y: i32 },
+        _Quit,
+        _Move { x: i32, y: i32 },
         Write(String),
-        ChangeColor(i32, i32, i32),
+        _ChangeColor(i32, i32, i32),
     }
-    
+
     // Also able to define methods on enums.
     impl Message {
         fn call(&self) {
-            println!("{:#?}", self);
+            match self {
+                Message::Write(text) => println!("Text: {}", text),
+                _ => println!("Other message"),
+            }
         }
     }
     let m = Message::Write(String::from("hello"));
-    // m.call();
+    m.call();
 
     // 2. Option
 
@@ -60,23 +64,23 @@ fn main() {
     - Using this type, the compiler can check whether you've handled all the cases, including when there is no value.
     - Unlike other languages, Rust doesn't use the null feature
         to avoid the bad case when you try to use a null value as a not-null value(ex. null pointer error).
-    - Instead, Rust use "None" variants in Option type. 
+    - Instead, Rust use "None" variants in Option type.
     */
     // Already defined in the prelude.
-    // enum Option<T> { 
+    // enum Option<T> {
     //     None,
     //     Some<T>,
     // }
 
-    let x = Some(5);
-    let y = Some('e');
-    let absent_value: Option<i32> = None; // Must include a type of the None variant.
+    let _x = Some(5);
+    let _y = Some('e');
+    let _absent_value: Option<i32> = None; // Must include a type of the None variant.
 
     // Why is having Option<T> any better than null?
     /*
     - Because Option<T> is able to check whether the value is None or not.
-    - When we have a normal type, the compile will ensure that we always have a vaild value of the type.
-    - When we have an Option type, the compile will check if the value is None or not before using that value.
+    - When we have a normal type, the compiler will ensure that we always have a vaild value of the type.
+    - When we have an Option type, the compiler will check if the value is None or not before using that value.
     */
 
     // 3. match: A Control Flow Construct
@@ -86,30 +90,31 @@ fn main() {
     - Rust has a match expression that allows us to do that.
     */
     #[derive(Debug)]
-    enum UsState {
+    enum _UsState {
         Alabama,
         Alaska,
         // ...
     }
 
-    enum Coin {
+    enum _Coin {
         Penny,
         Nickel,
         Dime,
-        Quarter(UsState),
+        Quarter(_UsState),
     }
 
     // Unlike `if`, `match` can evaluate any type.
-    fn value_in_cents(coin: Coin) -> u8 {
+    fn _value_in_cents(coin: _Coin) -> u8 {
         match coin {
             // There are arms: patterns and some code.
-            Coin::Penny => {
+            _Coin::Penny => {
                 println!("Lucky penny!");
                 1
             } // the comma is optional.
-            Coin::Nickel => 5,
-            Coin::Dime => 10,
-            Coin::Quarter(state) => {  // binding value 
+            _Coin::Nickel => 5,
+            _Coin::Dime => 10,
+            _Coin::Quarter(state) => {
+                // binding value
                 println!("State quarter from {:?}!", state);
                 25
             }
@@ -117,7 +122,7 @@ fn main() {
         // The resultant value of the matched expression becomes the resultant value of the match expression.
     }
 
-    // Note: Matches are exhaustive.    
+    // Note: Matches are exhaustive.
     // The arms' patterns must cover all the possible cases.
     // causes an error.
     // just fixed it!
@@ -130,7 +135,7 @@ fn main() {
 
     let dice_roll = 9;
     let dice_roll2 = 9;
-    
+
     match dice_roll {
         3 => add_fancy_hat(),
         7 => remove_fancy_hat(),
@@ -141,7 +146,7 @@ fn main() {
         3 => add_fancy_hat(),
         7 => remove_fancy_hat(),
         _ => reroll(), // is a placeholder.
-        // _ => (), // or do nothing.
+                       // _ => (), // or do nothing.
     }
 
     fn add_fancy_hat() {
@@ -159,5 +164,4 @@ fn main() {
     fn reroll() {
         println!("You rerolled!");
     }
-                
 }
