41d3f3cdf7834940a0c1d17b686103f221e53e8a
ITHwang <hwangintech@gmail.com>
2024-11-28 23:28:43 +0900

    finished ch19

---
+++ ch19_advanced_traits/src/lib.rs
@@ -0,0 +1,34 @@
+use std::fmt;
+use std::ops::Add;
+
+// impl Iterator for Counter {
+//     type Item = u32;
+
+//     fn next(&mut self) -> Option<Self::Item> {
+//         // --snip--
+//     }
+// }
+
+struct Millimeters(u32);
+struct Meters(u32);
+
+impl Add<Meters> for Millimeters {
+    type Output = Millimeters;
+
+    fn add(self, other: Meters) -> Millimeters {
+        Millimeters(self.0 + (other.0 * 1000))
+    }
+}
+
+pub trait OutlinePrint: fmt::Display {
+    fn outline_print(&self) {
+        let output = self.to_string();
+        let len = output.len();
+
+        println!("{}", "*".repeat(len + 4));
+        println!("*{}*", " ".repeat(len + 2));
+        println!("* {output} *");
+        println!("*{}*", " ".repeat(len + 2));
+        println!("{}", "*".repeat(len + 4));
+    }
+}
---
+++ ch19_advanced_traits/src/main.rs
@@ -0,0 +1,38 @@
+use ch19_advanced_traits::OutlinePrint;
+use std::fmt;
+use std::ops::Add;
+
+#[derive(Debug, Copy, Clone, PartialEq)]
+struct Point {
+    x: i32,
+    y: i32,
+}
+
+impl Add for Point {
+    type Output = Point;
+
+    fn add(self, other: Point) -> Point {
+        Point {
+            x: self.x + other.x,
+            y: self.y + other.y,
+        }
+    }
+}
+
+impl fmt::Display for Point {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "({}, {})", self.x, self.y)
+    }
+}
+
+impl OutlinePrint for Point {}
+
+fn main() {
+    assert_eq!(
+        Point { x: 1, y: 0 } + Point { x: 2, y: 3 },
+        Point { x: 3, y: 3 }
+    );
+
+    let p = Point { x: 1, y: 0 };
+    p.outline_print();
+}
